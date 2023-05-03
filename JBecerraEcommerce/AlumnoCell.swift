7173d7336a105f1c237f9b055960fb431529b849
joseRBP <josera.jack@gmail.com>
2023-05-03 12:57:57 -0600

    TableView

---
+++ JBecerraEcommerce/AlumnoCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlumnoCell.swift
+//  JBecerraEcommerce
+//
+//  Created by MacbookMBA7 on 03/05/23.
+//
+
+import UIKit
+
+class AlumnoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
