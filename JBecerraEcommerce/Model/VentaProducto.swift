9398190f40cfebb40a280c7d49eaca39e656679d
Joserbp <josera.jack@gmail.com>
2023-05-26 18:12:49 -0600

    Modelo VentaProducto CoreData Details

---
+++ JBecerraEcommerce/Model/VentaProducto.swift
@@ -0,0 +1,13 @@
+//
+//  VentaProducto.swift
+//  JBecerraEcommerce
+//
+//  Created by MacbookMBA7 on 26/05/23.
+//
+
+import Foundation
+
+class VentaProducto{
+    var alumno : Alumno? = nil
+    var cantidad : Int? = nil
+}
--- JBecerraEcommerce/ViewModel/CarritoViewModel.swift
+++ JBecerraEcommerce/ViewModel/CarritoViewModel.swift
@@ -56,11 +56,23 @@ class CarritoViewModel{
         do{
             let resultFetch = try context.fetch(response)
             for obj in resultFetch as! [NSManagedObject]{
-                let idVentaProducto = obj.value(forKey: "idProducto")
-                let cantidad = obj.value(forKey: "cantidad")
-                print(idProducto)
-                let result = GetByID(idVentaProducto)
-                print(cantidad)
+                let ventaProducto = VentaProducto()
+                
+                ventaProducto.alumno = Alumno()
+                ventaProducto.alumno?.IdAlumno = obj.value(forKey: "idProducto") as! Int
+                ventaProducto.cantidad = obj.value(forKey: "cantidad") as! Int
+                let resultGetById = AlumnoViewModel.GetById(idAlumno:ventaProducto.alumno?.IdAlumno as! Int)
+                if resultGetById.Correct! {
+                    let alumno = result.Object! as! Alumno
+                    
+                    ventaProducto.alumno?.Nombre = alumno.Nombre
+                    ventaProducto.alumno?.ApellidoPaterno = alumno.ApellidoPaterno
+                    
+                }
+                result.Objects?.append(ventaProducto)
+                
+                
+                
             }
         }
         catch let error {
